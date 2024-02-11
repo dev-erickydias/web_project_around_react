674dd00626d173af942c05345f9bd279cf456fbb
Ericky Dias <erickyhenriquesd@gmail.com>
2024-02-11 16:20:14 +0000

    add app jundo com potenciais modificaçoes ainda precvisa ser testadas

---
+++ src/components/App.js
@@ -0,0 +1,42 @@
+import Card from "./Card";
+import Header from "./Header"
+
+function App() {
+
+  const [isEditAvatarPopupOpen, setEditAvatarPopupOpen] = useState(false);
+  const [isEditProfilePopupOpen, setEditProfilePopupOpen] = useState(false);
+  const [isAddPlacePopupOpen,  setAddPlacePopupOpen] = useState(false);
+  const [selectedCard, setselectedCard] = useState(null);
+
+  const handleEditAvatarClick = () => {
+    setEditAvatarPopupOpen(true);
+  };
+
+  const handleEditProfileClick = () => {
+    setEditProfilePopupOpen(true);
+  };
+
+  const handleAddPlaceClick = () => {
+    setAddPlacePopupOpen(true);
+  };
+
+  const handleCardClick = (card) => {
+    setselectedCard(card);
+  }
+
+  const closeAllPopups = () => {
+    setAddPlacePopupOpen(false);
+    setEditProfilePopupOpen(false);
+    setEditAvatarPopupOpen(false);
+    setselectedCard(null);
+  }
+  
+  return (
+    <>
+    <Header />
+    <Card />
+    </>
+  );
+}
+
+export default App;
