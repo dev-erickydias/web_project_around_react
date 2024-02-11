8789e89bd0fe325d316f1f37b5bc034d5b4e5a3a
Ericky Dias <erickyhenriquesd@gmail.com>
2024-02-11 16:19:23 +0000

    add components header and your styles

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import "./header.css"
+import logo from "../image/Vector.svg"
+
+
+function Header (){
+  return (
+    <header className="header">
+    <img src={logo} alt="logo around the us" className="header__logo" />
+  </header>
+  )
+}
+export default Header
