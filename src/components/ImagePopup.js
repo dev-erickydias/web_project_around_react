07958ebe6df750ff5a69084f16cad2989ff2ca1e
Ericky Dias <erickyhenriquesd@gmail.com>
2024-02-28 08:40:32 +0000

    correção do css

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -6,12 +6,13 @@ function ImagePopup({ card, onClose }) {
       <div className="overlay" id="overlay-image"></div>
       <div className="popup-container">
         <div className="popup__image">
-          <img className="popup-container__image popup-container__image_card" id="popup-image" src={card?.link} alt={card?.name} />
-          <span className="popup__container-name">{card ? card.name : ''}</span>
-        </div>
         <div className="close-image">
           <img src={closeIcon} alt="Botão que feixa a caixa de edição" onClick={onClose} />
         </div>
+          <img className="popup-container__image popup-container__image_card" id="popup-image" src={card?.link} alt={card?.name} />
+          <span className="popup__container-name">{card ? card.name : ''}</span>
+        </div>
+        
       </div>
     </div>
   );
